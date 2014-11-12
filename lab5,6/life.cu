68668838449e64a393a44021cb9c27b616926279
alexbenfica <alexbenfica@gmail.com>
2014-11-12 18:27:48 -0200

    Still debugging with printf the copy from global do shared memory

--- lab5,6/life.cu
+++ lab5,6/life.cu
@@ -5,8 +5,11 @@
 #include "life_kernel.cu"
 
 
-int main(int argc, char ** argv)
-{
+
+
+
+// Without optimizing memory accessing...
+int life0(){
     // Definition of parameters
     int domain_x = 128;	// Multiple of threads_per_block * cells_per_word
     int domain_y = 128;
@@ -35,7 +38,7 @@ int main(int argc, char ** argv)
 
 	// Arrays of dimensions pitch * domain.y
 
-	init_kernel<<< grid, threads, 0 >>>(domain_gpu[0], pitch);
+	init_kernel0<<< grid, threads, 0 >>>(domain_gpu[0], pitch);
 
     // Timer initialization
     cudaEvent_t start, stop;
@@ -48,8 +51,8 @@ int main(int argc, char ** argv)
     // Kernel execution
     int shared_mem_size = 0;
     for(int i = 0; i < steps; i++) {
-	    life_kernel<<< grid, threads, shared_mem_size >>>(domain_gpu[i%2], domain_gpu[(i+1)%2], domain_x, domain_y, pitch);
-	}
+        life_kernel0<<< grid, threads, shared_mem_size >>>(domain_gpu[i%2], domain_gpu[(i+1)%2], domain_x, domain_y, pitch);
+    }
 
     // Stop timer
     CUDA_SAFE_CALL(cudaEventRecord(stop, 0));
@@ -93,6 +96,149 @@ int main(int argc, char ** argv)
     
     free(domain_cpu);
     
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Question 5 of lab: using shared memory to remove duplicate reads
+int life1(){
+    // Definition of parameters
+    
+    // Multiple of threads_per_block * cell_per_word
+    int domain_x = 128;
+    int domain_y = 128;
+    
+    // Total of 128 x 128 cells = 16384 cells
+    
+    int cells_per_word = 1;
+    int steps = 2;
+    
+    
+    
+    // CUDA grid dimensions    
+    // Max dimension size of a grid size    (x,y,z): (65535, 65535, 65535)    
+    int y_step = 4;
+    
+    int blocks_y = domain_y / y_step;
+    int blocks_x = 1;    
+    printf("\nGrid dimensions: %d x %d blocks\n", blocks_x, blocks_y);    
+    dim3  grid(blocks_x  , blocks_y );
+    
+    
+    // CUDA threads per block
+    //Max dimension size of a thread block (x,y,z): (1024, 1024, 64)
+    int thr_x = domain_x;
+    int thr_y = y_step;    
+
+    printf("Block dimensions: %d x %d threads\n", thr_x , thr_y);
+    dim3  threads(thr_x, thr_y);
+
+    // Allocation of arrays
+    int * domain_gpu[2] = {NULL, NULL};
+
+    size_t pitch;
+    CUDA_SAFE_CALL(cudaMallocPitch((void**)&domain_gpu[0], &pitch, domain_x / cells_per_word * sizeof(int), domain_y));
+    CUDA_SAFE_CALL(cudaMallocPitch((void**)&domain_gpu[1], &pitch, domain_x / cells_per_word * sizeof(int), domain_y));
+
+    //printf("%d", pitch);
+    
+    // Arrays of dimensions pitch * domain.y
+    init_kernel1<<< grid, threads, 0 >>>(domain_gpu[0],  domain_x, domain_y, pitch);
+
+    // Timer initialization
+    cudaEvent_t start, stop;
+    CUDA_SAFE_CALL(cudaEventCreate(&start));
+    CUDA_SAFE_CALL(cudaEventCreate(&stop));
+
+    // Start timer
+    CUDA_SAFE_CALL(cudaEventRecord(start, 0));
+
+    // Kernel execution
+    
+    int shared_mem_size = thr_x * (thr_y + 2) * sizeof(int) ;
+    printf("Shared mem size: %d bytes\n", shared_mem_size);
+    
+    for(int i = 0; i < steps; i++) {
+       life_kernel1<<< grid, threads, shared_mem_size >>>(domain_gpu[i%2], domain_gpu[(i+1)%2], domain_x, domain_y, pitch);
+    }
+
+    // Stop timer
+    CUDA_SAFE_CALL(cudaEventRecord(stop, 0));
+    CUDA_SAFE_CALL(cudaEventSynchronize(stop));
+
+    float elapsedTime;
+    CUDA_SAFE_CALL(cudaEventElapsedTime(&elapsedTime, start, stop));	// In ms
+    printf("GPU time: %f ms\n", elapsedTime);
+
+    CUDA_SAFE_CALL(cudaEventDestroy(start));
+    CUDA_SAFE_CALL(cudaEventDestroy(stop));
+
+    // Get results back
+    int * domain_cpu = (int*)malloc(pitch * domain_y);
+    CUDA_SAFE_CALL(cudaMemcpy(domain_cpu, domain_gpu[steps%2], pitch * domain_y, cudaMemcpyDeviceToHost));
+
+    CUDA_SAFE_CALL(cudaFree(domain_gpu[0]));
+    CUDA_SAFE_CALL(cudaFree(domain_gpu[1]));
+
+
+    // Count colors
+    int red = 0;
+    int blue = 0;
+    for(int y = 0; y < domain_y; y++){
+        for(int x = 0; x < domain_x; x++){
+            int cell = domain_cpu[y * pitch/sizeof(int) + x];
+            if(cell < 0) cell = 9;
+            printf("%u", cell % 10);
+            if(cell == 1) {
+                red++;
+            }
+            else if(cell == 2) {
+                blue++;
+            }
+        }
+        printf("\n");
+    }
+
+    printf("Red/Blue cells: %d/%d\n", red, blue);
+
+    free(domain_cpu);
+
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+int main(int argc, char ** argv){   
+    if(argc == 1){                
+        printf("You need to specify the implementation number!");
+        return 1;
+    }
+    
+    int implementation = atoi(argv[1]);        
+    
+    switch(implementation){
+        case 0: return life0();
+        case 1: return life1();
+    }
     return 0;
 }
 
--- lab5,6/life_kernel.cu
+++ lab5,6/life_kernel.cu
@@ -1,7 +1,4 @@
 
-__global__ void init_kernel(int * domain, int pitch){
-	domain[blockIdx.y * pitch / sizeof(int) + blockIdx.x * blockDim.x + threadIdx.x] = (1664525ul * (blockIdx.x + threadIdx.y + threadIdx.x) + 1013904223ul) % 3;
-}
 
 
 
@@ -24,16 +21,54 @@ __device__ int write_cell(int * dest_domain, int x, int y, int dx, int dy, int d
 
 
 
+__global__ void init_kernel0(int * domain, int pitch){
+    domain[blockIdx.y * pitch / sizeof(int) + blockIdx.x * blockDim.x + threadIdx.x] = (1664525ul * (blockIdx.x + threadIdx.y + threadIdx.x) + 1013904223ul) % 3;
+}
+
+
+
+
+__device__ int calc_color(int myself, int nb, int nr, int na){
+    
+    int color = myself;    
+    int tot_neig = nb + nr;   
+    
+    // Survival conditions
+    if(na == 1){
+        color = 0;
+    }
+    
+    if(tot_neig > 3){
+        color = 0;
+    }
+    
+    if(tot_neig == 3){    
+        if(myself == 0){
+            if(tot_neig == 3){
+                if(nb >= 2){
+                    color = 2;
+                }
+                else{
+                    color = 1;
+                }
+            }
+        }
+    }
+    return color;
+}
+
+
 
 // Compute kernel
-__global__ void life_kernel(int * source_domain, int * dest_domain,int domain_x, int domain_y, int pitch){
+__global__ void life_kernel0(int * source_domain, int * dest_domain,int domain_x, int domain_y, int pitch){
+    
+    
     int tx = blockIdx.x * blockDim.x + threadIdx.x;
     int ty = blockIdx.y;
     
-    // Read cell
-    int myself = read_cell(source_domain, tx, ty, 0, 0, domain_x, domain_y, pitch);
     
-    // TODO: Read the 8 neighbors and count number of blue and red
+    int myself = 0;
+
     int nr = 0; // number of red
     int nb = 0; // number of blue    
     int na = 0; // number of adjacent neighbours
@@ -44,7 +79,11 @@ __global__ void life_kernel(int * source_domain, int * dest_domain,int domain_x,
         for(int j=-1; j<2; j++){
             
             // skip the central element
-            if((i==0) && (j==0)) continue;            
+            if((i==0) && (j==0)) {
+                // Read cell
+                myself = read_cell(source_domain, tx, ty, 0, 0, domain_x, domain_y, pitch);
+                continue;            
+            }
             
             neig_value = read_cell(source_domain, tx, ty, i, j, domain_x, domain_y, pitch);
             
@@ -59,46 +98,188 @@ __global__ void life_kernel(int * source_domain, int * dest_domain,int domain_x,
         }        
     }
     
-
-    
-    
-    
-    // TODO: Compute new value       
-    // begins dead...
-    int new_value = myself;
-    
-    int tot_neig = nb + nr;
-    
-    
-    // Survival conditions
-    if(na == 1){
-        new_value = 0;
-    }
-    
-    if(tot_neig > 3){
-        new_value = 0;
-    }
-    
-    if(tot_neig == 3){    
-        if(myself == 0){
-            if(tot_neig == 3){
-                if(nb >= 2){
-                    new_value = 2;
-                }
-                else{
-                    new_value = 1;
-                }
-            }
-        }
-    }
-    
-    
-  
-    
-    //new_value = na;
+    int color = calc_color(myself, nb, nr, na);
     
     // TODO: Write it in dest_domain
-    write_cell(dest_domain, tx, ty, 0, 0, domain_x, domain_y, pitch, new_value);
-    
+    write_cell(dest_domain, tx, ty, 0, 0, domain_x, domain_y, pitch, color);    
 }
 
+
+
+
+
+
+
+
+
+
+
+
+__global__ void init_kernel1(int * domain, int domain_x, int domain_y, int pitch){
+    
+    int tx = threadIdx.x;
+    int ty = blockIdx.y * blockDim.y + threadIdx.y;    
+    
+    int value = threadIdx.x % 3;    
+    if(value != 2) value ^= 1 << 0;
+    write_cell(domain, tx, ty, 0 , 0 , domain_x, domain_y, pitch, value);                        
+}
+
+
+
+
+
+
+
+// Compute kernel
+__global__ void life_kernel1(int * source_domain, int * dest_domain, int domain_x, int domain_y, int pitch){
+    
+    extern __shared__ int sha[];
+
+#if 0
+    // Copies memory from global to shared. Each block copies 1 piece to its shared memory!
+    // Each thread copies a whole row from external memory.
+    // Only the first blockDim.y +2 threads are used to copy each row. 
+    // Total of blockDim.y +2 rows, given on extra row before and another after.    
+    if(threadIdx.x < blockDim.y + 2) {                       
+        
+        int ty = (blockIdx.y * blockDim.y + threadIdx.y - 1 + domain_y) % domain_y;                
+        
+        for(int j=0; j < blockDim.x; j++){                        
+            int isha = threadIdx.x * blockDim.x + j;
+            
+            sha[isha] = read_cell(source_domain, j, ty, 0, 0, domain_x, domain_y, pitch);            
+
+
+            
+            printf("\n   \npitch=%d  \nthreadIdx.x=%d   \nthreadIdx.y=%d  \nblockDim.x=%d  \nj = %d   \nty = %d    \nisha = %d   \nsha[isha] = %d", 
+                    pitch, 
+                    threadIdx.x, 
+                    threadIdx.y, 
+                    blockDim.x, 
+                    j,
+                    ty, 
+                    isha, 
+                    sha[isha]);
+            // TEST indexes: Copies shared memory back to to global memory!
+            //int value = sha[isha] + 1;
+            //if(blockIdx.y == 3)
+            //write_cell(dest_domain, j, ty, 0 , 0 , domain_x, domain_y, pitch, value);                        
+        }
+    }    
+#endif                       
+    
+
+    
+    int tx, ty, isha;
+    
+    
+    // Each thread copies one or two values form global to shared memory.
+    // First blockDim.x bytes has the values from line before.
+    // Last blockDim.x bytes has the values from line after.
+    isha = threadIdx.x + (threadIdx.y + 1) * blockIdx.x;
+    ty = blockIdx.y * blockDim.y + threadIdx.y;
+    sha[isha] = read_cell(source_domain, threadIdx.x, ty, 0, 0, domain_x, domain_y, pitch);            
+    
+    // copies the first line
+    if(!threadIdx.y){        
+        ty = ty + domain_y - 1;
+        ty %= domain_y;
+        //printf("\n%d", ty);
+        sha[threadIdx.x] = read_cell(source_domain, threadIdx.x, ty, 0, 0, domain_x, domain_y, pitch);            
+    }
+
+    // copies the last line
+    if(threadIdx.y == blockDim.y-1){        
+        ty += 1;
+        sha[isha + blockDim.x] = read_cell(source_domain, threadIdx.x, ty, 0, 0, domain_x, domain_y, pitch);            
+    }
+    
+
+    __syncthreads();        
+    
+    
+    
+            
+            
+    
+    if(blockIdx.y==0){    
+        if(threadIdx.x==0){    
+            if(threadIdx.y==0){    
+            
+                //printf("\n%d", blockDim.y);
+                //printf("\n%d\n", blockDim.x);
+
+                for(int j=0;j<blockDim.y+2;j++){
+                    for(int i=0;i<blockDim.x;i++){        
+                        int value = sha[i + j*blockDim.x];
+                        if((value < 3)&&(value >= 0)){                        
+                            printf("%d", value);
+                        }
+                        else{
+                            printf("-");
+                        }
+
+                    }
+                    printf("\n");        
+                }
+
+                printf("\n");        
+                printf("\n");        
+            
+            }
+        }
+    }    
+    
+
+    //write_cell(dest_domain, threadIdx.x, ty, 0 , 0 , domain_x, domain_y, pitch, sha[isha]);                        
+    
+    
+    __syncthreads();        
+    
+
+
+    int nr = 0; // number of red
+    int nb = 0; // number of blue    
+    int na = 0; // number of adjacent neighbours
+    
+    int myself;
+    int neig_value;
+    
+    for(int x = -1; x < 2; x++){        
+        for(int y = -1; y < 2; y++){            
+            
+            int sx = (threadIdx.x + blockDim.x + x) % blockDim.x;
+            int sy = (threadIdx.y + y + 1);
+            
+            int sindex = sx + sy * blockDim.x;
+            
+            // The central element is the cell itself
+            if((x==0) && (y==0)){
+                myself = sha[sindex];
+                continue;            
+            }            
+            
+            neig_value = sha[sindex];            
+            
+            if(neig_value == 1) nr++;
+            if(neig_value == 2) nb++;
+
+            if((x == 0) || (y == 0)){
+                if(neig_value > 0){
+                    na++;
+                }
+            }                
+        }        
+    }
+    
+    int color = calc_color(myself, nb, nr, na);
+    
+    tx = threadIdx.x;
+    ty = blockIdx.y * blockDim.y + threadIdx.y;    
+    
+    //color = sindex;
+    
+    
+    write_cell(dest_domain, tx, ty, 0, 0, domain_x, domain_y, pitch, color);
+}
